93df9d066e58eec45bd2e7f70ee6f215a78e1545
ilicevroman6-star <ilicevroman6@gmail.com>
2026-03-02 19:13:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import binascii
 import re
 import base64
 import codecs
 
 
-def decode_messages(file):
+def decode_messages(file: str):
     """
     Finds and decrypts messages
     Returns: {'base64': [], 'hex': [], 'rot13': []}
@@ -15,8 +16,10 @@ def decode_messages(file):
     base64_decoded = []
 
     for message in base64_strings:
-        base64message = base64.b64decode(message).decode('utf-8')
-        base64_decoded.append(base64message)
+        if len(message) % 4 == 0:
+            base64message = base64.b64decode(message).decode('utf-8')
+            base64_decoded.append(base64message)
+
 
     mask_2 = r'0x[A-Fa-f0-9]+'
 
@@ -27,7 +30,6 @@ def decode_messages(file):
         hex_message = codecs.decode(message[2:], 'hex').decode('utf-8')
         hex_decoded.append(hex_message)
 
-    # \s - находит и сохраняет при декодировании все пробельные символы
     mask_3 = r'\$[A-Za-z\s]+\$'
 
     rot13_strings = re.findall(mask_3, file)
@@ -44,7 +46,7 @@ def decode_messages(file):
            }
 
 
-def normalize_and_validate(file):
+def normalize_and_validate(file: str):
     """ Brings the data to a single format and verifies it
         Returns:  { 'phones': {'valid': [], 'invalid': []},
                     'dates': {'normalized': [], 'invalid': []},
@@ -54,8 +56,6 @@ def normalize_and_validate(file):
     pattern_1 = r'[+]?[78][- ]?\d{3}[- ]?\d{3}[- ]?\d{2}[- ]?\d{2}'
 
     valid = re.findall(pattern_1, file)
-
-
 
     invalid_dates = []
     pattern_2 = r'(?:\d{2}[/.]\d{2}[/.]\d{2,4})|(?:\d{4}[/-]\d{2}[/-]\d{2})'
@@ -84,36 +84,28 @@ def find_secrets(file: str):
     :return: list: a list of found secrets (API keys)
     """
 
-    try:
-        with open(file, 'r') as f:
-            text = f.read()
+    # Search keys.
+    regex_keys = r'(?:sk|pk)_(?:live|test)_[A-Za-z0-9]{25,255}'
+    keys = re.findall(regex_keys, file)
 
-        # Search keys.
-        regex_keys = r'(?:sk|pk)_(?:live|test)_[A-Za-z0-9]{25,255}'
-        keys = re.findall(regex_keys, text)
+    # Search passwords.
+    regex_passwords = r'[A-Za-z0-9!@#$%^&*()_+=\-]{8,}'
+    maybe_passwords = re.findall(regex_passwords, file)
 
-        # Search passwords.
-        regex_passwords = r'[A-Za-z0-9!@#$%^&*()_+=\-]{8,}'
-        maybe_passwords = re.findall(regex_passwords, text)
+    passwords = []
+    for element in maybe_passwords:
+        # Check letters, numbers and special symbols.
+        if (re.search(r'[A-Za-z]', element) and
+                re.search(r'[0-9]', element) and
+                re.search(r'[!@#$%^&*()_+\-=]', element)):
 
-        passwords = []
-        for element in maybe_passwords:
-            # Check letters, numbers and special symbols.
-            if (re.search(r'[A-Za-z]', element) and
-                    re.search(r'[0-9]', element) and
-                    re.search(r'[!@#$%^&*()_+\-=]', element)):
+            # Excludes not passwords.
+            if (not re.search(r'\d{4}-\d{2}-\d{2}', element) and
+            not re.match(r'^(sk|pk|rk)_', element.lower()) and
+            element not in passwords):
+                passwords.append(element)
+    return keys, passwords
 
-                # Excludes not passwords.
-                if not re.search(r'\d{4}-\d{2}-\d{2}', element):
-                    if not re.match(r'^(sk|pk|rk)_', element.lower()):
-                        if element not in passwords:
-                            passwords.append(element)
-
-        return keys, passwords
-
-    except FileNotFoundError:
-        print(f'Ошибка: файл {file} не найден')
-        return []
 
 
 
@@ -125,31 +117,30 @@ def find_system_info(file):
 
     result = {'ips': [], 'files': [], 'emails': []}
 
-    try:
-        with open(file, 'r', encoding='utf-8') as f:
-            text = f.read()
 
-        email_regex = r'[\w.-]+@[\w.-]+\.\w+'
-        result['emails'] = re.findall(email_regex, text)
 
-        ip_regex = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
-        ip_candidates = re.findall(ip_regex, text)
+    email_regex = r'[\w.-]+@[\w.-]+\.\w+'
+    result['emails'] = re.findall(email_regex, text)
 
-        for ip in ip_candidates:
-            number1, number2, number3, number4 = ip.split('.')
-            number1 = int(number1)
-            number2 = int(number2)
-            number3 = int(number3)
-            number4 = int(number4)
-            if (number1 <= 255 and number2 <= 255 and
-                number3 <= 255 and number4 <= 255):
-                result['ips'].append(ip)
+    ip_regex = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+    ip_candidates = re.findall(ip_regex, text)
+
+    for ip in ip_candidates:
+        number1, number2, number3, number4 = ip.split('.')
+        number1 = int(number1)
+        number2 = int(number2)
+        number3 = int(number3)
+        number4 = int(number4)
+        if (number1 <= 255 and number2 <= 255 and
+                    number3 <= 255 and number4 <= 255):
+            result['ips'].append(ip)
 
         file_regex = r'\b[\w.-]+\.(?:txt|log|ini|py|js)\b'
-        result['files'] = re.findall(file_regex, text, re.IGNORECASE)
+        result['files'] = re.findall(file_regex, text, re.IGNORECASE)  # Флаг, игнорирующий регистр
 
-        return result
-        
+    return result
+
+
 
 def analyze_logs(file):
     """
@@ -164,8 +155,7 @@ def analyze_logs(file):
         'failed_logins': []
     }
 
-    with open(file, 'r') as f:
-        text = f.read()
+
 
     # Mask for SQL-injections.
     sql_mask = r'OR\s+.*?=.*?|UNION\s+SELECT|DROP\s+TABLE|--|;\s*$'
@@ -246,34 +236,25 @@ def find_and_validate_credit_cards(file):
 
     return {'cards': {'valid': valid_cards, 'invalid': invalid_cards}}
 
+
 def generate_comprehensive_report(file):
     """ Генерирует полный отчет о расследовании """
-    report = { 'financial_data': find_and_validate_credit_cards(file),
-               'secrets': find_secrets(file),
-               'system_info': find_system_info(file),
-               'encoded_messages': decode_messages(file),
-               'security_threats': analyze_logs(file),
-               'normalized_data': normalize_and_validate(file)
-               }
+    report = {'financial_data': find_and_validate_credit_cards(file),
+              'secrets': find_secrets(file),
+              'system_info': find_system_info(file),
+              'encoded_messages': decode_messages(file),
+              'security_threats': analyze_logs(file),
+              'normalized_data': normalize_and_validate(file)
+              }
     return report
-    
+
+
+
 if __name__ == '__main__':
     try:
-        with open('data_leak_sample_2.txt', 'r', encoding = 'utf-8') as f:
+        with open('final_2.txt', 'r', encoding='utf-8') as f:
             text = f.read()
+            print(generate_comprehensive_report(text))
     except FileNotFoundError:
         print('File not found')
-        
-
-
-
-
-
-
-
-
-
-
-
-
 
